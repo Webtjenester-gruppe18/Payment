4ebb756fd35413c981e639707c0c0957082a560a
outrageousbog <olivrskoeppen@gmail.com>
2020-01-14 09:49:20 +0100

    setup controllers

---
+++ src/main/java/ws18/controllers/PaymentController.java
@@ -0,0 +1,14 @@
+package ws18.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    @RequestMapping("/payment")
+    public String index() {
+        return "Hello from paymentcontroller";
+    }
+
+}
--- src/test/java/ws18/PaymentApplicationTest.java
+++ src/test/java/ws18/PaymentApplicationTest.java
@@ -1,4 +1,4 @@
-package ws18.payment;
+package ws18;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
