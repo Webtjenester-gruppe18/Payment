4b781b5ed43bf1bdad7f638638133e6bc3b45486
Oliver <oliverskoeppen@gmail.com>
2020-01-14 06:53:25 +0100

    init commit

---
+++ src/test/java/ws18/payment/PaymentApplicationTest.java
@@ -0,0 +1,12 @@
+package ws18.payment;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
