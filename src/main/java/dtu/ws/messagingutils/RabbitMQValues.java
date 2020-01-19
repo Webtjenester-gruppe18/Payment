a0b6886f2abe729905e13a03426ff698bee6f0b9
Marcus August Christiansen <s175185@student.dtu.dk>
2020-01-19 13:09:12 +0100

    Update RabbitMQValues.java

--- src/main/java/dtu/ws/messagingutils/RabbitMQValues.java
+++ src/main/java/dtu/ws/messagingutils/RabbitMQValues.java
@@ -2,7 +2,7 @@ package dtu.ws.messagingutils;
 
 public class RabbitMQValues {
     public static final String TOPIC_EXCHANGE_NAME = "dtupay-eventsExchange";
-    static final String DTU_SERVICE_ROUTING_KEY = "dtupay";
+    public static final String DTU_SERVICE_ROUTING_KEY = "dtupay";
     static final String PAYMENT_SERVICE_QUEUE_NAME = "paymentservice-queue";
     public static final String TOKEN_SERVICE_ROUTING_KEY = "token";
     public static final String USER_SERVICE_ROUTING_KEY = "user";
